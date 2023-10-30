3599477085bfcd883a7e74438fecc20af9556fe6
Trevor Childs <trevorchilds8@gmail.com>
2023-10-30 10:52:49 -0600

    Started database with MongoDB

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,20 +1,24 @@
-const getAllTasks = (req,res)=>{
-    res.send('get all tasks');
-}
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
 
-const createTask = (req,res)=>{
-    res.send('create task');
-}
-const getTask = (req,res)=>{
-    res.send('get single task');
-}
-const updateTask = (req,res)=>{
-    res.send('update task');
-}
-const deleteTask = (req,res)=>{
-    res.send('delete task');
-}
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
 
 module.exports = {
-    getAllTasks, createTask, getTask, updateTask, deleteTask
-}
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,3 @@
+const connectionString =
+  "mongodb+srv://ironreplica:<password>@nodeexpressprojects.dzkwflb.mongodb.net/?retryWrites=true&w=majority";
+// 52:37
