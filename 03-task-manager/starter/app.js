2bd80d8542d0c5d10f16309f069508abbdd2d198
ironreplica <trevorchilds8@gmail.com>
2023-10-29 21:47:19 -0600

    Started Task Manager App

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+//middleware
+
+app.use(express.json());
+
+//routes
+// get all the tasks
+app.get(`/hello`, (req, res)=>{
+    res.send('Task manager app');
+});
+app.use('/api/v1/tasks',tasks);
+
+// create a new task
+// get a single task (/:id)
+// update a task (/:id)
+// delete a task (/:id)
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}`)); 
