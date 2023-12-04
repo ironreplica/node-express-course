c907170a294caffcc070f0a1631e73f56e2c1bd8
Trevor Childs <trevorchilds8@gmail.com>
2023-12-04 10:46:01 -0700

    3:54:39

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,14 @@
-const getAllProductsStatic = async (req,res) =>{
-    throw new Error('Testing async errors');
-    res.status(200).json({msg:'products testing route'})
-}
-const getAllProducts = async (req,res) =>{
-    res.status(200).json({msg:'products route'})
-}
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({});
+  res.status(200).json({ products });
+};
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
 
 module.exports = {
-    getAllProductsStatic,
-    getAllProducts,
-}
+  getAllProductsStatic,
+  getAllProducts,
+};
